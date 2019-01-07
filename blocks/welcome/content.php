24cb40cdf085cdbe1272dff44433fa75b2fa6360
Sumner <serhard2556@gmail.com>
2019-01-06 20:45:33 -0700

    Layout styles for welcome block complete

--- blocks/welcome/content.php
+++ blocks/welcome/content.php
@@ -13,26 +13,23 @@ $button = get_field('buttons');
 
 <div class="virtuoso-block front_page_top_container welcome">
     <div class="front_page_top_wrap">
+        <div class="heading_wrap">
+          <div class="image_wrap"><img alt="logo" src="<?php echo $image?>"/></div>
+          <h1><?php bloginfo('title'); ?></h1>
+          <h4><?php bloginfo('description');?></h4>
+        </div>
+      <div class="button_container">
 
-  <div class="image_wrap">
-    <img src="<?php echo $image?>"/>
-  </div>
+        <?php while( have_rows('buttons') ): the_row(); ?>
 
-  <h1><?php bloginfo('title'); ?></h1>
-  <h4><?php bloginfo('description');?></h4>
-  <div class="button_container">
+          <?php $link = get_sub_field('link'); ?>
+          <?php $text = get_sub_field('text'); ?>
+          <?php $icon = get_sub_field('icon'); ?>
+          <div class='link_wrap'>
+            <a class="button" href="<?php echo $link ?>"><?php echo $text; ?> <?php echo $icon; ?></a>
+          </div>
 
-    <?php while( have_rows('buttons') ): the_row(); ?>
-
-      <?php $link = get_sub_field('link'); ?>
-      <?php $text = get_sub_field('text'); ?>
-      <?php $icon = get_sub_field('icon'); ?>
-      <div class='link_wrap'>
-        <a class="button" href="<?php echo $link ?>"><?php echo $text; ?> <?php echo $icon; ?></a>
-      </div>
-
-    <?php endwhile; ?>
-
+        <?php endwhile; ?>
       </div>
     </div>
 </div>
