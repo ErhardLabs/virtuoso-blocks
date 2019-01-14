8670438d4e07d130fcbd8823f6511a2e22659016
Sumner <serhard2556@gmail.com>
2019-01-14 16:05:14 -0700

    Updated markup and styles for latest posts, offset image, welcome, and bio

--- blocks/bio/content.php
+++ blocks/bio/content.php
@@ -11,23 +11,25 @@ $socialMediaLinks = get_field('social_media_links');
 $bioText = get_field('bio_text');
 
 ?>
-<div class="virtuoso-block bio_wrap">
-  <div class="image_wrap">
-    <img src="<?php echo $image ?>"/>
-  </div>
-  <div class="social_media_wrap">
-    <?php
+<div class="virtuoso-block bio_container bio">
+    <div class="bio_wrap">
+      <div class="image_wrap">
+        <img src="<?php echo $image ?>"/>
+      </div>
+      <div class="social_media_wrap">
+        <?php
 
-    if ( $socialMediaLinks ) {
+        if ( $socialMediaLinks ) {
 
-	    foreach ( $socialMediaLinks as $media ) {
-		    ?><a class="button" href="<?php echo $media['link'] ?>"><?php echo $media['icon'] ?></a><?php
-	    }
-    }
-    ?>
-  </div>
-  <div class="bio_text_wrap">
-    <p><?php echo $bioText ?></p>
-  </div>
+            foreach ( $socialMediaLinks as $media ) {
+                ?><a class="social-button" href="<?php echo $media['link'] ?>"><?php echo $media['icon'] ?></a><?php
+            }
+        }
+        ?>
+      </div>
+      <div class="bio_text_wrap">
+        <p><?php echo $bioText ?></p>
+      </div>
+    </div>
 </div>
 <?php
