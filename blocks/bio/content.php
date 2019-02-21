5b520aad8637db9749de8cbc7be0613395fd8945
Sumner <serhard2556@gmail.com>
2019-02-20 22:10:13 -0700

    Updated team grid mobile responsivness on more than two members

--- blocks/bio/content.php
+++ blocks/bio/content.php
@@ -6,6 +6,7 @@
  * This is the template that displays the testimonial block.
  */
 
+$bioTitle = get_field('bio_title');
 $image = get_field('image');
 $socialMediaLinks = get_field('social_media_links');
 $bioText = get_field('bio_text');
@@ -13,11 +14,16 @@ $bioText = get_field('bio_text');
 ?>
 <div class="virtuoso-block bio_container bio">
     <div class="bio_wrap">
+	    <?php if ($bioTitle): ?>
+        <h2><?php echo $bioTitle; ?></h2>
+		<?php endif; ?>
         <?php if ($image): ?>
       <div class="image_wrap">
         <img src="<?php echo $image ?>"/>
       </div>
         <?php endif; ?>
+
+	    <?php if ($socialMediaLinks): ?>
       <div class="social_media_wrap">
         <?php
 
@@ -29,6 +35,7 @@ $bioText = get_field('bio_text');
         }
         ?>
       </div>
+        <?php endif; ?>
       <div class="bio_text_wrap">
         <p><?php echo $bioText ?></p>
       </div>
